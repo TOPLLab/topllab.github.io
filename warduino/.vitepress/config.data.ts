b846f3bd20bcd614b020e1395f560fc0b9071a05
tolauwae <Tom.Lauwaerts@UGent.be>
2024-03-19 16:12:55 +0100

    Bump versions

--- warduino/.vitepress/config.data.ts
+++ warduino/.vitepress/config.data.ts
@@ -2,10 +2,13 @@ export default {
   load() {
     return {
         vm: {
-            version: '0.4.3'
+            version: '0.4.4'
         },
         plugin: {
             version: '0.4.1'
+        },
+        latch: {
+            version: '0.2.1'
         }
     }
   }
