05f2c78468193352376d8b0d1bc3faf13e0b475d
tolauwae <Tom.Lauwaerts@UGent.be>
2025-01-20 11:37:54 +0100

    Update software versions

--- warduino/.vitepress/config.data.ts
+++ warduino/.vitepress/config.data.ts
@@ -2,13 +2,13 @@ export default {
   load() {
     return {
         vm: {
-            version: '0.5.1'
+            version: '0.6.1'
         },
         plugin: {
             version: '0.4.1'
         },
         latch: {
-            version: '0.2.1'
+            version: '0.3.1'
         }
     }
   }
